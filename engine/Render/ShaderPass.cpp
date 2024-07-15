103804a91e75416bff1abba65d77a0cccc72440c
Lee <li20020410@126.com>
2024-07-15 20:21:49 +0800

    Rewrite error handling

--- engine/Render/ShaderPass.cpp
+++ engine/Render/ShaderPass.cpp
@@ -2,6 +2,7 @@
 #include <format>
 #include <cassert>
 #include <stdexcept>
+#include <iostream>
 
 namespace Engine
 {
@@ -18,7 +19,7 @@ namespace Engine
         return m_shaderProgram;
     }
 
-    void ShaderPass::Use() const
+    void ShaderPass::Use() const noexcept
     {
         assert(this->IsLinked() && "Trying to use unlinked program");
         glUseProgram(m_shaderProgram);
@@ -37,55 +38,93 @@ namespace Engine
         }
 
         GLenum glError = GL_NO_ERROR;
+        GLint success = GL_TRUE;
 
         GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        assert(glIsShader(vertexShader) == GL_TRUE);
+        glError = glGetError();
+        if (glError != GL_NO_ERROR) {
+            std::cerr << std::format("Failed to create vertex shader, OpenGL error {}", (int)glError) << std::endl;
+            return false;
+        }
 
         glShaderSource(vertexShader, 1, &vertex, nullptr);
         glError = glGetError();
         if (glError != GL_NO_ERROR) {
-            throw std::runtime_error(std::format("Failed to set vertex shader source, OpenGL error {}", (int)glError));
+            std::cerr << std::format("Failed to set vertex shader source, OpenGL error {}", (int)glError) << std::endl;
             return false;
         }
 
         glCompileShader(vertexShader);
-        glError = glGetError();
-        if (glError != GL_NO_ERROR) {
-            throw std::runtime_error(std::format("Failed to compile vertex shader, OpenGL error {}", (int)glError));
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (success != GL_TRUE) {
+            GLchar log[128];
+            glGetShaderInfoLog(vertexShader, sizeof log, nullptr, log);
+            std::cerr 
+                << std::format("Failed to compile vertex shader, log: {}", log) 
+                << std::endl;
             return false;
         }
 
         GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-        assert(glIsShader(fragmentShader) == GL_TRUE);
-
-        glShaderSource(fragmentShader, 1, &fragment, nullptr);
-        glCompileShader(fragmentShader);
         glError = glGetError();
         if (glError != GL_NO_ERROR) {
-            throw std::runtime_error(std::format("Failed to compile fragment shader, OpenGL error {}", (int)glError));
+            std::cerr << std::format("Failed to create fragment shader, OpenGL error {}", (int)glError) << std::endl;
+            return false;
+        }
+
+        glShaderSource(fragmentShader, 1, &fragment, nullptr);
+        glError = glGetError();
+        if (glError != GL_NO_ERROR) {
+            std::cerr 
+                << std::format("Failed to set fragment shader source, OpenGL error {}", (int)glError) 
+                << std::endl;
+            return false;
+        }
+
+        glCompileShader(fragmentShader);
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (success != GL_TRUE) {
+            GLchar log[128];
+            glGetShaderInfoLog(fragmentShader, sizeof log, nullptr, log);
+            std::cerr 
+                << std::format("Failed to compile fragment shader, log: {}", log) 
+                << std::endl ;
             return false;
         }
 
         m_shaderProgram = glCreateProgram();
+        glError = glGetError();
+        if (glError != GL_NO_ERROR) {
+            std::cerr 
+                << std::format("Failed to create program, OpenGL error {}", (int)glError) 
+                << std::endl ;
+            m_shaderProgram = 0;
+            return false;
+        }
+
         glAttachShader(m_shaderProgram, vertexShader);
         glAttachShader(m_shaderProgram, fragmentShader);
         glLinkProgram(m_shaderProgram);
-        glError = glGetError();
-
         glDetachShader(m_shaderProgram, vertexShader);
         glDetachShader(m_shaderProgram, fragmentShader);
         glDeleteShader(vertexShader);
         glDeleteShader(fragmentShader);
 
-        if (glError != GL_NO_ERROR) {
+        glGetProgramiv(m_shaderProgram, GL_LINK_STATUS, &success);
+        if (success != GL_TRUE) {
+            GLchar log[128];
+            glGetProgramInfoLog(m_shaderProgram, sizeof log, nullptr, log);
+            std::cerr 
+                << std::format("Failed to link program, log: {}", log) 
+                << std::endl ;
+
             m_shaderProgram = 0;
-            throw std::runtime_error(std::format("Failed to link program, OpenGL error {}", (int)glError));
             return false;
         }
         return true;
     }
 
-    GLuint ShaderPass::GetUniform(const char * name) const noexcept
+    GLint ShaderPass::GetUniform(const char * name) const noexcept
     {
         static_assert(std::is_same_v<GLchar, char> == true, "GLchar and char should be the same type.");
         assert(this->IsLinked() && "Trying to get uniform location from uncompiled program.");
