c0cc25467379e7dfb412e34535efdb1e91e152ea
Lee <li20020410@126.com>
2024-07-18 00:53:00 +0800

    Improve NativeResource interface design

--- engine/Render/NativeResource/NativeResource.cpp
+++ engine/Render/NativeResource/NativeResource.cpp
@@ -1,15 +1,29 @@
 #include "NativeResource.h"
-
-#ifndef NDEBUG
-#include <iostream>
-#endif
+#include <cassert>
 
 namespace Engine {
     NativeResource::~NativeResource()
     {
-#ifndef NDEBUG
-        std::cerr << "GPU native resource with handle " << m_handle << " destructed." << std::endl;
-#endif
+        assert(m_handle == 0 && "A native resource is destructed without releasing.");
+    }
+
+    NativeResource::NativeResource(NativeResource && other)
+    {
+        // Set m_handle to avoid spurious destruction.
+        other.m_handle = 0;
+    }
+
+    NativeResource & NativeResource::operator=(NativeResource && other)
+    {
+        assert(&other != this && "Self move assignment detected.");
+
+        // Steal the handle from other.
+        this->Release();
+        this->m_handle = other.m_handle;
+
+        // Set m_handle to avoid spurious destruction.
+        other.m_handle = 0;
+        return *this;
     }
 
     GLuint NativeResource::GetHandle() const noexcept
