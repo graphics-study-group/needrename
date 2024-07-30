055246b66ff0bc6c889500de72d4e1f7a18a78e0
Lee <li20020410@126.com>
2024-07-30 20:48:14 +0800

    Refactor Transform and add docs

--- engine/Core/Math/Transform.h
+++ engine/Core/Math/Transform.h
@@ -9,37 +9,20 @@ namespace Engine
     class Transform
     {
     public:
-        Transform()
-            : m_position(0.0f)
-            , m_rotation(0.0f)
-            , m_scale(1.0f)
-        {
-        }
+        /// @brief Get the matrix representing the transform
+        /// the matrix is constructed such that
+        /// ret = scale * location * rotation
+        /// @return transform matrix 
+        glm::mat4 GetModelMatrix() const;
 
-        glm::mat4 GetModelMatrix() const
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, m_position);
-            model = glm::rotate(model, glm::radians(m_rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(m_rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(m_rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
-            model = glm::scale(model, m_scale);
-            return model;
-        }
-
-        Transform operator*(const Transform & other) const
-        {
-            Transform result;
-            result.m_position = m_position + other.m_position;
-            result.m_rotation = m_rotation + other.m_rotation;
-            result.m_scale = m_scale * other.m_scale;
-            return result;
-        }
-
+        /// @brief Apply transform to another transform
+        /// @param other 
+        /// @return 
+        Transform operator*(const Transform & other) const;
     public:
-        glm::vec3 m_position;
-        glm::vec3 m_rotation;
-        glm::vec3 m_scale;
+        glm::vec3 m_position {0.0f};
+        glm::vec3 m_rotation {0.0f};
+        glm::vec3 m_scale {1.0f};
     };
 }
 
