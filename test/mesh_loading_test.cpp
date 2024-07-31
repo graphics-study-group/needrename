5bb9562000b8f6cc8deb9047f06d50d213cebbe9
Lee <li20020410@126.com>
2024-07-31 19:59:06 +0800

    Automate more tests

--- test/mesh_loading_test.cpp
+++ test/mesh_loading_test.cpp
@@ -12,23 +12,32 @@ Engine::MainClass * cmc;
 
 int main(int argc, char * argv[])
 {
-    assert(argc == 2);
-
     SDL_Init(SDL_INIT_VIDEO);
+    
+    if(argc < 2) {
+        SDL_LogCritical(0, "Mesh not provided.");
+        return -1;
+    }
+    SDL_LogInfo(0, "Loading mesh %s", argv[1]);
+
     StartupOptions opt;
+    opt.resol_x = 800;
+    opt.resol_y = 600;
     cmc = new Engine::MainClass(
             SDL_INIT_VIDEO,
             SDL_LOG_PRIORITY_VERBOSE
     );
-    SDLWindow::EnableMSAA(4);
     cmc->Initialize(&opt);
-    glEnable(GL_DEPTH_TEST);
+
     std::shared_ptr<GameObject> go;
     std::shared_ptr<MeshComponent> testMesh = std::make_shared<MeshComponent>(go);
-    testMesh->ReadAndFlatten(argv[1]);
+
+    std::filesystem::path mesh {argv[1]};
+    mesh = mesh / "mesh.obj";
+    if(!testMesh->ReadAndFlatten(mesh))
+        return -1;
 
     delete cmc;
-
     return 0;
 }
 
