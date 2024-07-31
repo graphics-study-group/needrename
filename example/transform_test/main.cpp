adb02ec953651bb75cde5e587d8338840ef5650b
Lee <li20020410@126.com>
2024-07-31 14:29:18 +0800

    Add transform_test/main.cpp

---
+++ example/transform_test/main.cpp
@@ -0,0 +1,47 @@
+#include <SDL3/SDL.h>
+#include <cassert>
+#include <random>
+
+#include "Core/Math/Transform.h"
+
+using namespace Engine;
+
+std::mt19937 gen{};
+std::uniform_real_distribution <float> translation_dist {-100.0, 100.0};
+std::uniform_real_distribution <float> rotation_dist {-360.0, 360.0};
+std::uniform_real_distribution <float> scale_dist {0.1, 10.0};
+
+Transform RandomTransform () {
+    glm::vec3 translation{translation_dist(gen), translation_dist(gen), translation_dist(gen)};
+    glm::vec3 rotation{rotation_dist(gen), rotation_dist(gen), rotation_dist(gen)};
+    glm::vec3 scale{scale_dist(gen), scale_dist(gen), scale_dist(gen)};
+
+    Transform t;
+    t.SetPosition(translation).SetRotationEuler(rotation).SetScale(scale);
+    return t;
+}
+
+int main(int argc, char * argv[])
+{
+    Transform t1, t2;
+    for (int i = 0; i < 100; i++) {
+        t1 = RandomTransform();
+        t2 = RandomTransform();
+
+        Transform t3 = t2 * t1;
+        glm::mat4 m1, m2, m3;
+
+        m1 = t1.GetModelMatrix();
+        m2 = t2.GetModelMatrix();
+        m3 = t3.GetModelMatrix();
+
+        glm::vec4 origin {0.0, 0.0, 0.0, 1.0};
+        glm::vec4 v1 = m2 * m1 * origin;
+        glm::vec4 v2 = m3 * origin;
+
+        printf("v1: %f, %f, %f, %f\t", v1.x, v1.y, v1.z, v1.w);
+        printf("v2: %f, %f, %f, %f\n", v2.x, v2.y, v2.z, v2.w);
+        assert(glm::distance(v1, v2) <= 1e-3);
+    }
+    return 0;
+}
