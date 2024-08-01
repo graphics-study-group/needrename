6d458694fdfa5e31dedaf25f571d650b7d8e63f3
Lee <li20020410@126.com>
2024-08-02 00:05:04 +0800

    Implement CameraComponent partially

---
+++ engine/Framework/component/RenderComponent/CameraComponent.cpp
@@ -0,0 +1,47 @@
+#include "CameraComponent.h"
+
+#include "Framework/go/GameObject.h"
+
+#include <SDL3/SDL.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+namespace Engine{
+    CameraComponent::CameraComponent(std::weak_ptr<GameObject> gameObject) : Component(gameObject)
+    {
+    }
+
+    void CameraComponent::Tick(float)
+    {
+    }
+
+    glm::mat4 CameraComponent::GetViewMatrix() const
+    {
+        auto parent = m_parentGameObject.lock();
+        if (!parent) {
+            SDL_LogWarn(0, "Missing parent game object for camera");
+            return glm::mat4(1.0f);
+        }
+        Transform transform = parent->GetWorldTransform();
+        glm::mat4 tmat = transform.GetTransformMatrix();
+
+        glm::vec4 origin{0.0f, 0.0f, 0.0f, 1.0f};
+        origin = tmat * origin;
+
+        // Y axis points front, so we just transform (0,1,0) to get local front vector
+        glm::vec4 front{0.0f, 1.0f, 0.0f, 1.0f};
+        front = tmat * front;
+
+        return glm::lookAtRH(glm::vec3{origin}, glm::vec3{front}, glm::vec3{0.0, 0.0, 1.0});
+    }
+
+    glm::mat4 CameraComponent::GetProjectionMatrix() const
+    {
+        return glm::mat4();
+    }
+
+    CameraContext CameraComponent::CreateContext() const
+    {
+        return CameraContext{this->GetViewMatrix(), this->GetProjectionMatrix()};
+    }
+};
