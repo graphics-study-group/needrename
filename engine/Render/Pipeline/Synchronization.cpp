9bbfd4c87aaf1ce0d6c938a3e49f880d2efd1a9d
Lee <li20020410@126.com>
2024-08-10 21:28:06 +0800

    Add CommandBuffer and Synchronization

---
+++ engine/Render/Pipeline/Synchronization.cpp
@@ -0,0 +1,37 @@
+#include "Synchronization.h"
+#include "Render/RenderSystem.h"
+#include <SDL3/SDL.h>
+
+namespace Engine
+{
+    Synchronization::Synchronization(const RenderSystem & system) : m_system(system) {
+        SDL_LogInfo(SDL_LOG_CATEGORY_RENDER, "Creating synchronization primitives.");
+    }
+
+    TwoStageSynchronization::TwoStageSynchronization(const RenderSystem & system) : Synchronization(system) {
+        const auto & device = system.getDevice();
+
+        m_imageAvailable = device.createSemaphoreUnique({});
+        m_renderFinished = device.createSemaphoreUnique({});
+        m_inflight = device.createFenceUnique({vk::FenceCreateFlagBits::eSignaled});
+    }
+
+    vk::Semaphore TwoStageSynchronization::GetNextImageSemaphore() const {
+        return m_imageAvailable.get();
+    }
+
+std::vector<vk::Semaphore> TwoStageSynchronization::GetCommandBufferWaitSignals(uint32_t) const {
+        return {m_imageAvailable.get()};
+    }
+    std::vector<vk::PipelineStageFlags> TwoStageSynchronization::GetCommandBufferWaitSignalFlags(uint32_t) const {
+        return {vk::PipelineStageFlagBits::eColorAttachmentOutput};
+    }
+    std::vector<vk::Semaphore> TwoStageSynchronization::GetCommandBufferSigningSignals(uint32_t) const {
+        return {m_renderFinished.get()};
+    }
+    vk::Fence TwoStageSynchronization::GetCommandBufferFence(uint32_t) const {
+        return {m_inflight.get()};
+    }
+} // namespace Engine
+
+
