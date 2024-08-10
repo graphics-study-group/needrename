2106887be70956900042151d8c0128174abb6543
CaptainHarryChen <captainharrychen@qq.com>
2024-08-10 21:46:07 +0800

    separate the obj loader from the asset manager

--- engine/Asset/AssetManager/AssetManager.cpp
+++ engine/Asset/AssetManager/AssetManager.cpp
@@ -7,8 +7,7 @@
 #include <cereal/cereal.hpp>
 #include <cereal/archives/binary.hpp>
 
-#include "Asset/Mesh/Mesh.h"
-#include "Render/Material/Shadeless.h"
+#include "Asset/Loader/ObjLoader.h"
 
 namespace Engine
 {
@@ -49,7 +48,8 @@ namespace Engine
         std::string extension = resourcePath.extension().string();
         if (extension == ".obj")
         {
-            LoadObjResource(resourcePath, path_in_project);
+            ObjLoader loader;
+            loader.LoadObjResource(resourcePath, path_in_project);
         }
         else
         {
@@ -83,199 +83,4 @@ namespace Engine
             throw std::runtime_error("asset GUID already exists");
         m_assets[guid] = path;
     }
-
-    void AssetManager::LoadObjResource(const std::filesystem::path &path, const std::filesystem::path &path_in_project)
-    {
-        tinyobj::ObjReader reader;
-        reader.ParseFromFile(path.string());
-        if (!reader.Error().empty())
-        {
-            throw std::runtime_error("TinyObjReader reports errors reading " + path.string());
-        }
-
-        if (!reader.Warning().empty())
-        {
-            std::cerr << "TinyObjReader reports warnings reading " << path.string() << std::endl;
-            std::cerr << "\t" << reader.Warning() << std::endl;
-        }
-
-        const tinyobj::attrib_t &attrib = reader.GetAttrib();
-        const std::vector<tinyobj::shape_t> &shapes = reader.GetShapes();
-        const std::vector<tinyobj::material_t> &materials = reader.GetMaterials();
-
-        GUID mesh_guid = GenerateGUID();
-        nlohmann::json mesh_json;
-        std::vector<GUID> material_guids;
-        Mesh assetMesh;
-
-        std::unordered_map<std::string, GUID> texture_path_guid_map;
-        for (size_t i = 0; i < materials.size(); i++)
-        {
-            GUID mat_guid;
-            LoadObjMaterialResource(texture_path_guid_map, path.parent_path(), materials[i], path_in_project, mat_guid);
-            material_guids.push_back(mat_guid);
-        }
-
-        // Save mesh data to binary file
-        assetMesh.LoadFromTinyobj(attrib, shapes);
-        std::filesystem::path mesh_data_path = GetAssetsDirectory() / path_in_project / (path.stem().string() + ".mesh_data");
-        std::ofstream os(mesh_data_path, std::ios::binary);
-        cereal::BinaryOutputArchive oarchive(os);
-        oarchive(assetMesh);
-
-        // Save mesh meta file
-        mesh_json["guid"] = GUIDToString(mesh_guid);
-        mesh_json["name"] = path.stem().string();
-        mesh_json["type"] = "Mesh";
-        mesh_json["submesh_count"] = assetMesh.GetSubmeshCount();
-
-        std::filesystem::path mesh_json_path = GetAssetsDirectory() / path_in_project / (path.stem().string() + ".mesh_data.asset");
-        std::ofstream mesh_file(mesh_json_path);
-        if (mesh_file.is_open())
-        {
-            mesh_file << mesh_json.dump(4);
-            mesh_file.close();
-            AddAsset(mesh_guid, path_in_project / mesh_json_path.stem());
-        }
-        else
-        {
-            throw std::runtime_error("Failed to open mesh file");
-        }
-
-        // Save mesh prefab file
-        nlohmann::json prefab_json;
-        GUID prefab_guid = GenerateGUID();
-        prefab_json["guid"] = GUIDToString(prefab_guid);
-        prefab_json["name"] = path.stem().string();
-        prefab_json["type"] = "GameObject";
-        prefab_json["components"] = nlohmann::json::array();
-        nlohmann::json mesh_component_json;
-        mesh_component_json["type"] = "MeshComponent";
-        mesh_component_json["mesh"] = GUIDToString(mesh_guid);
-        mesh_component_json["materials"] = nlohmann::json::array();
-        for (size_t s = 0; s < shapes.size(); s++)
-        {
-            // XXX: every face in a shape must have the same material
-            size_t material_id = shapes[s].mesh.material_ids[0];
-            mesh_component_json["materials"].push_back(GUIDToString(material_guids[material_id]));
-        }
-        prefab_json["components"].push_back(mesh_component_json);
-        
-        std::filesystem::path prefab_json_path = GetAssetsDirectory() / path_in_project / (path.stem().string() + ".prefab.asset");
-        std::ofstream prefab_file(prefab_json_path);
-        if (prefab_file.is_open())
-        {
-            prefab_file << prefab_json.dump(4);
-            prefab_file.close();
-            AddAsset(prefab_guid, path_in_project / prefab_json_path.stem());
-        }
-        else
-        {
-            throw std::runtime_error("Failed to open prefab file");
-        }
-    }
-
-    void AssetManager::LoadObjMaterialResource(std::unordered_map<std::string, GUID> &texture_path_guid_map, const std::filesystem::path &parent_directory, const tinyobj::material_t &material, const std::filesystem::path &path_in_project, GUID &guid)
-    {
-        guid = GenerateGUID();
-        nlohmann::json material_json;
-        material_json["guid"] = GUIDToString(guid);
-        material_json["name"] = material.name;
-        material_json["type"] = "Shadeless";
-
-        material_json["ambient"] = {material.ambient[0], material.ambient[1], material.ambient[2]};
-        material_json["diffuse"] = {material.diffuse[0], material.diffuse[1], material.diffuse[2]};
-        material_json["specular"] = {material.specular[0], material.specular[1], material.specular[2]};
-        material_json["transmittance"] = {material.transmittance[0], material.transmittance[1], material.transmittance[2]};
-        material_json["emission"] = {material.emission[0], material.emission[1], material.emission[2]};
-        material_json["shininess"] = material.shininess;
-        material_json["ior"] = material.ior;
-        material_json["dissolve"] = material.dissolve;
-        material_json["illum"] = material.illum;
-        material_json["roughness"] = material.roughness;
-        material_json["metallic"] = material.metallic;
-        material_json["sheen"] = material.sheen;
-        material_json["clearcoat_thickness"] = material.clearcoat_thickness;
-        material_json["clearcoat_roughness"] = material.clearcoat_roughness;
-        material_json["anisotropy"] = material.anisotropy;
-        material_json["anisotropy_rotation"] = material.anisotropy_rotation;
-
-        GUID tex_guid;
-        // XXX: texture options are not implemented
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.ambient_texname, path_in_project, tex_guid))
-            material_json["ambient_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.diffuse_texname, path_in_project, tex_guid))
-            material_json["diffuse_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.specular_texname, path_in_project, tex_guid))
-            material_json["specular_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.specular_highlight_texname, path_in_project, tex_guid))
-            material_json["specular_highlight_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.bump_texname, path_in_project, tex_guid))
-            material_json["bump_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.displacement_texname, path_in_project, tex_guid))
-            material_json["displacement_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.alpha_texname, path_in_project, tex_guid))
-            material_json["alpha_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.reflection_texname, path_in_project, tex_guid))
-            material_json["reflection_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.roughness_texname, path_in_project, tex_guid))
-            material_json["roughness_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.metallic_texname, path_in_project, tex_guid))
-            material_json["metallic_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.sheen_texname, path_in_project, tex_guid))
-            material_json["sheen_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.emissive_texname, path_in_project, tex_guid))
-            material_json["emissive_tex"] = GUIDToString(tex_guid);
-        if(LoadObjTextureResource(texture_path_guid_map, parent_directory, material.normal_texname, path_in_project, tex_guid))
-            material_json["normal_tex"] = GUIDToString(tex_guid);
-
-        std::filesystem::path json_path = GetAssetsDirectory() / path_in_project / (material.name + ".asset");
-        std::ofstream material_file(json_path);
-        if (material_file.is_open())
-        {
-            material_file << material_json.dump(4);
-            material_file.close();
-            AddAsset(guid, path_in_project / json_path.stem());
-        }
-        else
-        {
-            throw std::runtime_error("Failed to open material file");
-        }
-    }
-
-    bool AssetManager::LoadObjTextureResource(std::unordered_map<std::string, GUID> &texture_path_guid_map, const std::filesystem::path &parent_directory, const std::string &filename, const std::filesystem::path &path_in_project, GUID &guid)
-    {
-        if(filename.empty())
-            return false;
-        if(texture_path_guid_map.find(filename) != texture_path_guid_map.end())
-        {
-            guid = texture_path_guid_map[filename];
-            return true;
-        }
-
-        guid = GenerateGUID();
-        nlohmann::json texture_json;
-        texture_json["guid"] = GUIDToString(guid);
-        texture_json["name"] = filename;
-        texture_json["type"] = "ImmutableTexture2D";
-
-        std::filesystem::path texpath_in_project = GetAssetsDirectory() / path_in_project / filename;
-        std::filesystem::path tex_origin_path = parent_directory / filename;
-        std::filesystem::copy_file(tex_origin_path, texpath_in_project);
-        std::filesystem::path json_path = texpath_in_project.replace_filename(texpath_in_project.filename().string() + ".asset");
-        std::ofstream tex_file(json_path);
-        if (tex_file.is_open())
-        {
-            tex_file << texture_json.dump(4);
-            tex_file.close();
-            AddAsset(guid, path_in_project / json_path.stem());
-        }
-        else
-        {
-            throw std::runtime_error("Failed to open texture file");
-        }
-        texture_path_guid_map[filename] = guid;
-        return true;
-    }
-
 } // namespace Engine
--- engine/Asset/AssetManager/AssetManager.h
+++ engine/Asset/AssetManager/AssetManager.h
@@ -51,37 +51,14 @@ namespace Engine
         inline std::filesystem::path GetProjectPath() const { return m_projectPath; }
         inline std::filesystem::path GetAssetsDirectory() const { return m_projectPath / "assets"; }
 
+        void AddAsset(const GUID &guid, const std::filesystem::path &path);
+
     protected:
         static std::mt19937_64 m_guid_gen;
 
         std::filesystem::path m_projectPath;
         /// @brief GUID to asset path map
         std::unordered_map<GUID, std::filesystem::path, GUIDHash> m_assets;
-
-        void AddAsset(const GUID &guid, const std::filesystem::path &path);
-
-    private:
-        /// @brief Load an external obj resource, copy to the project asset directory, and create a meta file
-        /// @param path path to the external obj resource
-        /// @param path_in_project path to the output asset directory relative to the project asset directory
-        void LoadObjResource(const std::filesystem::path &path, const std::filesystem::path &path_in_project);
-
-        /// @brief Load an external material resource, read from .mtl file associated with an obj file. create a meta file at @param path_in_project
-        /// @param texture_path_guid_map to mark the texture files that have been loaded, prevent loading the same texture multiple times
-        /// @param parent_directory path to the parent directory of the obj file
-        /// @param material material data read from tinyobjloader
-        /// @param path_in_project path to the output asset directory relative to the project asset directory
-        /// @param guid to store the GUID of the material
-        void LoadObjMaterialResource(std::unordered_map<std::string, GUID> &texture_path_guid_map, const std::filesystem::path &parent_directory, const tinyobj::material_t &material, const std::filesystem::path &path_in_project, GUID &guid);
-
-        /// @brief Load an external texture resource, read from .mtl file associated with an obj file. create a meta file at @param path_in_project
-        /// @param texture_path_guid_map to mark the texture files that have been loaded, prevent loading the same texture multiple times
-        /// @param parent_directory path to the parent directory of the obj file
-        /// @param filename file name of the texture file
-        /// @param path_in_project path to the output asset directory relative to the project asset directory
-        /// @param guid to store the GUID of the texture
-        /// @return true if the texture is loaded successfully
-        bool LoadObjTextureResource(std::unordered_map<std::string, GUID> &texture_path_guid_map, const std::filesystem::path &parent_directory, const std::string &filename, const std::filesystem::path &path_in_project, GUID &guid);
     };
 } // namespace Engine
 
