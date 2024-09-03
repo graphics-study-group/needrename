e811bde616e5b1aafe1eba8dccdf2083d57b9d79
Lee <li20020410@126.com>
2024-09-03 23:27:00 +0800

    Add AllocatedImage2D

---
+++ engine/Render/Memory/Image2D.cpp
@@ -0,0 +1,85 @@
+#include "Image2D.h"
+
+#include "Render/RenderSystem.h"
+
+namespace Engine {
+    vk::MemoryPropertyFlags AllocatedImage2D::GetMemoryProperty(ImageType) {
+        return vk::MemoryPropertyFlagBits::eDeviceLocal;
+    }
+
+    vk::ImageUsageFlags AllocatedImage2D::GetImageUsage(ImageType type) {
+        switch (type) {
+        case ImageType::DepthStencilImage:
+        case ImageType::DepthImage:
+            return vk::ImageUsageFlagBits::eDepthStencilAttachment;
+        case ImageType::TextureImage:
+            return vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eTransferDst;
+        }
+    }
+
+    vk::ImageAspectFlags AllocatedImage2D::GetImageAspect(ImageType type) {
+        switch (type) {
+        case ImageType::DepthImage:
+            return vk::ImageAspectFlagBits::eDepth;
+        case ImageType::DepthStencilImage:
+            return vk::ImageAspectFlagBits::eDepth | vk::ImageAspectFlagBits::eStencil;
+        case ImageType::TextureImage:
+            return vk::ImageAspectFlagBits::eColor;
+        }
+    }
+
+    AllocatedImage2D::AllocatedImage2D(std::weak_ptr<RenderSystem> system) : m_system(system) {
+    }
+
+    void AllocatedImage2D::Create(uint32_t width, uint32_t height, ImageType type, vk::Format format, uint32_t mip) {
+        vk::ImageCreateInfo info{
+            vk::ImageCreateFlags{},
+            vk::ImageType::e2D,
+            format,
+            {width, height, 1},
+            mip,
+            1,
+            vk::SampleCountFlagBits::e1,
+            vk::ImageTiling::eOptimal,
+            GetImageUsage(type),
+            vk::SharingMode::eExclusive,
+            {},
+            vk::ImageLayout::eUndefined
+        };
+        auto system = m_system.lock();
+        auto device = system->getDevice();
+        m_image = device.createImageUnique(info);
+
+        vk::MemoryRequirements req = device.getImageMemoryRequirements(m_image.get());
+        vk::MemoryAllocateInfo minfo {
+            req.size, system->FindPhysicalMemory(req.memoryTypeBits, GetMemoryProperty(type))
+        };
+        m_memory = device.allocateMemoryUnique(minfo);
+        device.bindImageMemory(m_image.get(), m_memory.get(), 0);
+
+        vk::ImageViewCreateInfo vinfo {
+            vk::ImageViewCreateFlags{},
+            m_image.get(),
+            vk::ImageViewType::e2D,
+            format,
+            vk::ComponentMapping {},
+            vk::ImageSubresourceRange {
+                GetImageAspect(type),
+                0, mip, 0, 1
+            }
+        };
+        m_view = device.createImageViewUnique(vinfo);
+    }
+
+    vk::Image AllocatedImage2D::GetImage() const {
+        return m_image.get();
+    }
+
+    vk::DeviceMemory AllocatedImage2D::GetMemory() const {
+        return m_memory.get();
+    }
+
+    vk::ImageView AllocatedImage2D::GetImageView() const {
+        return m_view.get();
+    }
+};
