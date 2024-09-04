c8eba621919cda9c199e259f654de3c23467d31a
Lee <li20020410@126.com>
2024-09-04 21:11:27 +0800

    Add per camera constant data

---
+++ engine/Render/ConstantData/PerCameraConstants.h
@@ -0,0 +1,34 @@
+#ifndef RENDER_CONSTANTDATA_PERCAMERACONSTANTS_INCLUDED
+#define RENDER_CONSTANTDATA_PERCAMERACONSTANTS_INCLUDED
+
+#include <glm.hpp>
+#include "Render/VkWrapper.tcc"
+#include "Render/Memory/Buffer.h"
+
+namespace Engine {
+    class RenderSystem;
+
+    namespace ConstantData {
+        struct PerCameraStruct {
+            glm::mat4 view_matrix;
+            glm::mat4 proj_matrix;
+        };
+
+        class PerCameraConstantLayout : public VkWrapperIndependent<vk::UniqueDescriptorSetLayout> {
+        protected:
+            static constexpr std::array <vk::DescriptorSetLayoutBinding, 1> BINDINGS = {
+                vk::DescriptorSetLayoutBinding{
+                    0,
+                    vk::DescriptorType::eUniformBuffer,
+                    1,
+                    vk::ShaderStageFlagBits::eVertex
+                }
+            };
+        public:
+            static constexpr uint32_t PER_CAMERA_SET_NUMBER = 0;
+            void Create(vk::Device device);
+        };
+    }
+}
+
+#endif // RENDER_CONSTANTDATA_PERCAMERACONSTANTS_INCLUDED
---
+++ engine/Render/RenderSystem/DescriptorPool.h
@@ -0,0 +1,37 @@
+#ifndef RENDER_RENDERSYSTEM_DESCRIPTORPOOL_INCLUDED
+#define RENDER_RENDERSYSTEM_DESCRIPTORPOOL_INCLUDED
+
+#include "Render/ConstantData/PerCameraConstants.h"
+
+namespace Engine {
+    namespace RenderSystemState {
+        /// @brief A descriptor pool and related infrastructures for per-view global constants like camera matrices
+        class GlobalConstantDescriptorPool : public Engine::VkWrapperIndependent <vk::UniqueDescriptorPool> {
+        protected:
+            static constexpr uint32_t MAX_SET_SIZE = 16;
+            static constexpr std::array <vk::DescriptorPoolSize, 1> DESCRIPTOR_POOL_SIZES = {
+                vk::DescriptorPoolSize{vk::DescriptorType::eUniformBuffer, 64}
+            };
+
+            // Per camera constant descriptor set layout
+            Engine::ConstantData::PerCameraConstantLayout m_per_camera_constant_layout{};
+            // Per camera constant uniform buffers
+            std::vector <Engine::Buffer> m_per_camera_buffers {};
+            // Per camera constant descriptor sets. These sets don't need explicit freeing.
+            std::vector <vk::DescriptorSet> m_per_camera_descriptor_sets {};
+            // Mapped memories for buffers
+            std::vector <std::byte *> m_per_camera_memories {};
+
+            void CreateLayouts(std::shared_ptr <RenderSystem> system);
+            void AllocateGlobalSets(std::shared_ptr <RenderSystem> system, uint32_t inflight_frame_count);
+
+        public:
+            void Create(std::weak_ptr <RenderSystem> system, uint32_t inflight_frame_count);
+            auto GetPerCameraConstantLayout() const -> const decltype(m_per_camera_constant_layout) &;
+            auto GetPerCameraConstantSet(uint32_t inflight) const -> const decltype(m_per_camera_descriptor_sets[inflight]) &;
+            std::byte * GetPerCameraConstantMemory(uint32_t inflight) const;
+        };
+    }
+}
+
+#endif // RENDER_RENDERSYSTEM_DESCRIPTORPOOL_INCLUDED
