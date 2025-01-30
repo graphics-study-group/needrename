16ee2bf81bc8326feed6b9798a82291c02767e99
Vincent Lee <vincent_li_2024@126.com>
2025-01-31 00:07:47 +0800

    Add UBO variable saving

---
+++ engine/Render/Material/MaterialInstance.cpp
@@ -0,0 +1,42 @@
+#include "MaterialInstance.h"
+#include "MaterialTemplateUtils.h"
+#include <SDL3/SDL.h>
+
+namespace Engine {
+    MaterialInstance::MaterialInstance(
+        std::weak_ptr <RenderSystem> system, 
+        std::shared_ptr<MaterialTemplate> tpl
+        ) : m_system(system), m_parent_template(tpl)
+    {
+        // Allocate uniform buffers and per-material descriptor sets
+        for (const auto & [idx, info] : tpl->GetAllPassInfo()) {
+            PassInfo pass{Buffer{system}, {}};
+            pass.desc_set = tpl->AllocateDescriptorSet(idx);
+
+            auto ubo_size = tpl->GetMaximalUBOSize(idx);
+            if (ubo_size == 0) {
+                SDL_LogWarn(SDL_LOG_CATEGORY_RENDER, "Found zero-sized UBO when processing pass %ull of material", ubo_size);
+            } else {
+                pass.ubo.Create(Buffer::BufferType::Uniform, tpl->GetMaximalUBOSize(idx));
+            }
+
+            m_pass_info[idx] = std::move(pass);
+        }
+    }
+
+    void MaterialInstance::WriteUBOUniform(uint32_t pass, uint32_t index, std::any uniform)
+    {
+        assert(
+            MaterialTemplateUtils::REGISTERED_SHADER_UNIFORM_TYPES.find(uniform.type()) 
+            != MaterialTemplateUtils::REGISTERED_SHADER_UNIFORM_TYPES.end()
+            && "Unsupported uniform type."
+        );
+        assert(m_pass_info.find(pass) != m_pass_info.end() && "Cannot find pass.");
+        assert(
+            m_parent_template.lock()->GetVariable(index, pass).location.set
+            && "Cannot find uniform in designated pass."
+        );
+
+        m_variables[pass][index] = uniform;
+    }
+}
--- engine/Render/Material/MaterialInstance.h
+++ engine/Render/Material/MaterialInstance.h
@@ -2,26 +2,39 @@
 #define RENDER_MATERIAL_MATERIALINSTANCE_INCLUDED
 
 #include "MaterialTemplate.h"
+#include "Render/Memory/Buffer.h"
 #include <any>
 
 namespace Engine
 {
-    /// @brief An instance of a given material,
+    /// @brief A light-weight instance of a given material,
     /// where all mutable data such as texture and uniforms are stored.
     class MaterialInstance {
+    public:
+        struct PassInfo {
+            Buffer ubo;
+            vk::DescriptorSet desc_set;
+        };
+
+    private:
+        std::weak_ptr <RenderSystem> m_system;
+        std::weak_ptr <MaterialTemplate> m_parent_template;
+        std::unordered_map <uint32_t, std::unordered_map<uint32_t, std::any>> m_variables {};
+        std::unordered_map <uint32_t, PassInfo> m_pass_info {};
     
     public:
-        MaterialInstance(std::weak_ptr <MaterialTemplate> tpl);
+        MaterialInstance(std::weak_ptr <RenderSystem> system, std::shared_ptr <MaterialTemplate> tpl);
 
         /// @brief Set the texture uniform descriptor to point to a given texture.
         /// @param name 
         /// @param texture 
-        void WriteTextureUniform(const char * name, const AllocatedImage2DTexture & texture);
+        // void WriteTextureUniform(const std::string & name, const AllocatedImage2DTexture & texture);
     
         /// @brief Set the uniform variable descriptor to point to a given value.
-        /// @param name 
+        /// @param pass
+        /// @param index 
         /// @param uniform 
-        void WriteUBOUniform(const char * name, std::any uniform);
+        void WriteUBOUniform(uint32_t pass, uint32_t index, std::any uniform);
 
         /// @brief Write descriptors to GPU.
         /// Should be called before drawing.
