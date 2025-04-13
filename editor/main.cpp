0a3467a1becddc45ee8682fad0eff54d9f8a1edd
Captain Harry Chen <captainharrychen@qq.com>
2025-04-14 00:22:32 +0800

    render image in editor

--- editor/main.cpp
+++ editor/main.cpp
@@ -1,81 +1,108 @@
 #include <SDL3/SDL.h>
 #include <cassert>
+#include <iostream>
 #include <fstream>
-#include <chrono>
 
-#include "MainClass.h"
-#include "Functional/SDLWindow.h"
-#include "Framework/component/RenderComponent/MeshComponent.h"
-#include "Render/Pipeline/RenderTarget/RenderTargetSetup.h"
-#include "Render/RenderSystem.h"
-#include "GUI/GUISystem.h"
+#include <cmake_config.h>
+#include <MainClass.h>
+#include <Functional/SDLWindow.h>
+#include <Render/RenderSystem.h>
+#include <Render/AttachmentUtils.h>
+#include <Render/Memory/Image2DTexture.h>
+#include <Render/Memory/Buffer.h>
+#include <Render/Pipeline/CommandBuffer.h>
+#include <Render/RenderSystem/Swapchain.h>
+#include <Render/RenderSystem/FrameManager.h>
+#include <Framework/world/WorldSystem.h>
+#include <Asset/AssetManager/AssetManager.h>
 
 using namespace Engine;
-namespace sch = std::chrono;
 
-int main(int argc, char ** argv)
+int main()
 {
-    int64_t max_frame_count = std::numeric_limits<int64_t>::max();
-    if (argc > 1) {
-        max_frame_count = std::atoll(argv[1]);
-        if (max_frame_count == 0) return -1;
-    }
+    std::filesystem::path project_path(ENGINE_PROJECTS_DIR);
+    project_path = project_path / "test_project";
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    StartupOptions opt{.resol_x = 1920, .resol_y = 1080, .title = "Vulkan Test"};
+    StartupOptions opt{.resol_x = 1920, .resol_y = 1080, .title = "Project Loading Test"};
 
     auto cmc = MainClass::GetInstance();
     cmc->Initialize(&opt, SDL_INIT_VIDEO, SDL_LOG_PRIORITY_VERBOSE);
-
+    cmc->GetAssetManager()->SetBuiltinAssetPath(std::filesystem::path(ENGINE_BUILTIN_ASSETS_DIR));
+    cmc->GetAssetManager()->LoadBuiltinAssets();
     auto rsys = cmc->GetRenderSystem();
-    // rsys->EnableDepthTesting();
-    auto gsys = cmc->GetGUISystem();
+    auto asset_manager = cmc->GetAssetManager();
+    auto world = cmc->GetWorldSystem();
 
-    RenderTargetSetup rts{rsys};
-    rts.CreateFromSwapchain();
-    rts.SetClearValues({
-        vk::ClearValue{vk::ClearColorValue{0.0f, 0.0f, 0.0f, 1.0f}},
-        vk::ClearValue{vk::ClearDepthStencilValue{1.0f, 0U}}
-    });
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Loading project");
+    cmc->LoadProject(project_path);
 
-    bool quited = false;
-    while(max_frame_count--) {
+    Engine::AllocatedImage2D color{rsys}, depth{rsys};
+    color.Create(1920, 1080, Engine::ImageUtils::ImageType::ColorAttachment, Engine::ImageUtils::ImageFormat::R8G8B8A8SRGB, 1);
+    depth.Create(1920, 1080, Engine::ImageUtils::ImageType::DepthImage, Engine::ImageUtils::ImageFormat::D32SFLOAT, 1);
+
+    Engine::AttachmentUtils::AttachmentDescription color_att, depth_att;
+    color_att.image = color.GetImage();
+    color_att.image_view = color.GetImageView();
+    color_att.load_op = vk::AttachmentLoadOp::eClear;
+    color_att.store_op = vk::AttachmentStoreOp::eStore;
+
+    depth_att.image = depth.GetImage();
+    depth_att.image_view = depth.GetImageView();
+    depth_att.load_op = vk::AttachmentLoadOp::eClear;
+    depth_att.store_op = vk::AttachmentStoreOp::eDontCare;
+
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Entering main loop");
+
+    SDL_Event event;
+    bool onQuit = false;
+
+    unsigned int FPS_TIMER = 0;
+    float current_time = SDL_GetTicks();
+    float start_time = current_time;
+
+    while (true)
+    {
+        asset_manager->LoadAssetsInQueue();
+
+        bool quited = false;
         SDL_Event event;
-        while(SDL_PollEvent(&event) != 0) {
-            switch(event.type) {
+        while (SDL_PollEvent(&event) != 0)
+        {
+            switch (event.type)
+            {
             case SDL_EVENT_QUIT:
                 quited = true;
                 break;
             }
-            gsys->ProcessEvent(&event);
         }
-        
-        gsys->PrepareGUI();
-        ImGui::ShowDemoWindow();
+        if (quited)
+            break;
 
+        float current_time = SDL_GetTicks();
+        float dt = (current_time - FPS_TIMER) / 1000.0f;
+        FPS_TIMER = current_time;
+        world->Tick(dt);
+
+        // Draw
         auto index = rsys->StartFrame();
-        RenderCommandBuffer & cb = rsys->GetCurrentCommandBuffer();
+        RenderCommandBuffer &cb = rsys->GetCurrentCommandBuffer();
 
-        assert(index < 3);
-    
         cb.Begin();
-        vk::Extent2D extent {rsys->GetSwapchain().GetExtent()};
-        cb.BeginRendering(rts, extent, index);
-        gsys->DrawGUI(cb);
+        vk::Extent2D extent{rsys->GetSwapchain().GetExtent()};
+        cb.BeginRendering(color_att, depth_att, extent, index);
+        rsys->DrawMeshes();
         cb.EndRendering();
         cb.End();
         cb.Submit();
-
+        rsys->GetFrameManager().CopyToFramebuffer(color.GetImage());
         rsys->CompleteFrame();
-
-        SDL_Delay(10);
-
-        if (quited) break;
     }
-
     rsys->WaitForIdle();
     rsys->ClearComponent();
+
+    SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, "Unloading Main-class");
 
     return 0;
 }
