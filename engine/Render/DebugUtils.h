619e2a23c7feacdec020128a85757f3b60441f88
Vincent Lee <vincent_li_2024@126.com>
2025-05-16 22:43:15 +0800

    Use macro definition instead of global function.

--- engine/Render/DebugUtils.h
+++ engine/Render/DebugUtils.h
@@ -6,12 +6,14 @@
 #define DEBUG_CMD_START_LABEL(cmd, ...) (cmd.beginDebugUtilsLabelEXT(vk::DebugUtilsLabelEXT{__VA_ARGS__}))
 #define DEBUG_CMD_END_LABEL(cmd) (cmd.endDebugUtilsLabelEXT())
 #define DEBUG_SET_NAME(device, type, obj, name) (device.setDebugUtilsObjectNameEXT(vk::DebugUtilsObjectNameInfoEXT{type, (uint64_t)obj, name}))
+#define DEBUG_SET_NAME_TEMPLATE(device, obj, name) (DEBUG_SET_NAME_TEMPLATE_IMPL(device, obj, name))
 
 #else
 
-#define DEBUG_CMD_START_LABEL(cmd, ...)
-#define DEBUG_CMD_END_LABEL(cmd)
-#define DEBUG_SET_NAME(device, type, obj, name)
+#define DEBUG_CMD_START_LABEL(cmd, ...) (void(0))
+#define DEBUG_CMD_END_LABEL(cmd) (void(0))
+#define DEBUG_SET_NAME(device, type, obj, name) (void(0))
+#define DEBUG_SET_NAME_TEMPLATE(device, obj, name) (void(0))
 
 #endif
 
@@ -19,8 +21,9 @@ template <typename T>
 concept vulkan_hpp_handle = std::same_as<decltype(T::objectType), const vk::ObjectType> && std::convertible_to<typename T::CType, void *>;
 
 template <vulkan_hpp_handle T>
-void DEBUG_SET_NAME_TEMPLATE(vk::Device device, T handle, const std::string & name) {
+void DEBUG_SET_NAME_TEMPLATE_IMPL(vk::Device device, T handle, const std::string & name) {
 #ifndef NDEBUG
+    if (name.empty())   return;
     device.setDebugUtilsObjectNameEXT(
         vk::DebugUtilsObjectNameInfoEXT{
             T::objectType,
@@ -32,8 +35,9 @@ void DEBUG_SET_NAME_TEMPLATE(vk::Device device, T handle, const std::string & na
 }
 
 template <vulkan_hpp_handle T>
-void DEBUG_SET_NAME_TEMPLATE(vk::Device device, T handle, const char * name) {
+void DEBUG_SET_NAME_TEMPLATE_IMPL(vk::Device device, T handle, const char * name) {
 #ifndef NDEBUG
+    if (strlen(name) == 0)  return;
     device.setDebugUtilsObjectNameEXT(
         vk::DebugUtilsObjectNameInfoEXT{
             T::objectType,
