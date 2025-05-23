476cedcd2d3eca44f29181ba87f8a2c00985ae76
Lee <vincent_li_2024@126.com>
2025-05-23 21:40:32 +0800

    Adjust ImageAccessType enum.
    See https://registry.khronos.org/vulkan/specs/latest/man/html/VkAccessFlagBits2.html for details.

--- engine/Render/Pipeline/CommandBuffer/ICommandContext.h
+++ engine/Render/Pipeline/CommandBuffer/ICommandContext.h
@@ -7,21 +7,26 @@ namespace Engine {
     class ICommandBuffer;
     class ICommandContext {
     public:
-        enum class AccessType {
+        enum class ImageAccessType {
             None,
-            // Read by fragment shaders, etc.
-            Read,
-            // Write by graphics as attachments or transfer commands.
-            Write,
-            // Random write by compute shaders.
-            RandomWrite
-        };
-
-        enum class ContextType {
-            None,
-            Graphics,
-            Compute,
-            Transfer
+            /// Read by transfer command, in transfer source layout.
+            TransferRead,
+            /// Read by rasterization pipeline in attachment load operation, in color attachment layout.
+            /// @warning Disregarded for now.
+            ColorAttachmentRead,
+            /// Read by rasterization pipeline in attachment load operation, in depth attachment layout.
+            /// @warning Disregarded for now.
+            DepthAttachmentWrite,
+            /// Read by shader, in shader readonly layout.
+            ShaderRead,
+            /// Write by rasterization pipeline color output stage, in color attachment layout.
+            ColorAttachmentWrite,
+            /// Write by rasterization pipeline depth test stage, in depth attachment layout.
+            DepthAttachmentWrite,
+            /// Write by transfer command, in transfer destination layout.
+            TransferWrite,
+            /// Random write in shaders as storage image, in general layout.
+            ShaderRandomWrite,
         };
 
         ICommandContext() = default;
@@ -34,17 +39,15 @@ namespace Engine {
 
         virtual void UseImage(
             vk::Image & img,
-            AccessType currentAccess,
-            AccessType previousAccess,
-            ContextType previousCtx
+            ImageAccessType currentAccess,
+            ImageAccessType previousAccess
         ) noexcept = 0;
 
         /* virtual void UseImage (
             vk::Image & img,
             vk::SubresourceRange range,
-            AccessType currentAccess,
-            AccessType previousAccess,
-            ContextType previousCtx
+            ImageAccessType currentAccess,
+            ImageAccessType previousAccess
         ) noexcept = 0; */
 
         virtual void PrepareCommandBuffer () = 0;
