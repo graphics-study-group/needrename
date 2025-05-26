416247fd5e4819dd4966fd03aa04ac953a1b8363
Lee <vincent_li_2024@126.com>
2025-05-26 21:26:34 +0800

    Add stub implementation for ComputeContext.

--- engine/Render/Pipeline/CommandBuffer/ComputeContext.cpp
+++ engine/Render/Pipeline/CommandBuffer/ComputeContext.cpp
@@ -1,10 +1,45 @@
 #include "ComputeContext.h"
 
+#include "Render/Pipeline/CommandBuffer/AccessHelperFuncs.h"
+
 namespace Engine {
+
+    struct ComputeContext::impl {
+        ComputeCommandBuffer cb;
+        std::vector <vk::ImageMemoryBarrier2> barriers {};
+        impl(ComputeCommandBuffer &&cb_) : cb(std::move(cb_)) {}
+    };
+
     void ComputeContext::UseImage(vk::Image img, ImageAccessType currentAccess, ImageAccessType previousAccess) noexcept
     {
     }
+
+    ComputeContext::ComputeContext(
+        ComputeCommandBuffer &&cb
+    ) : pimpl(std::make_unique<ComputeContext::impl>(std::move(cb)))
+    {
+    }
+    ComputeContext::~ComputeContext() = default;
+
     void ComputeContext::UseImage(vk::Image img, ImageComputeAccessType currentAccess, ImageAccessType previousAccess) noexcept
     {
+        switch(currentAccess) {
+            case ImageComputeAccessType::ShaderRandomWrite:
+                UseImage(img, ImageAccessType::ShaderRandomWrite, previousAccess);
+                break;
+            case ImageComputeAccessType::ShaderRead:
+                UseImage(img, ImageAccessType::ShaderRead, previousAccess);
+                break;
+            case ImageComputeAccessType::ShaderReadRandomWrite:
+                UseImage(img, ImageAccessType::ShaderReadRandomWrite, previousAccess);
+                break;
+        }
+    }
+
+    void ComputeContext::PrepareCommandBuffer()
+    {
+        if (pimpl->barriers.empty())    return;
+        vk::DependencyInfo dep{vk::DependencyFlags{0}, {}, {}, pimpl->barriers};
+        this->GetCommandBuffer().GetCommandBuffer().pipelineBarrier2(dep);
     }
 }
--- engine/Render/Pipeline/CommandBuffer/ComputeContext.h
+++ engine/Render/Pipeline/CommandBuffer/ComputeContext.h
@@ -2,12 +2,18 @@
 #define PIPELINE_COMMANDBUFFER_COMPUTECONTEXT_INCLUDED
 
 #include "Render/Pipeline/CommandBuffer/ICommandContext.h"
+#include "Render/Pipeline/CommandBuffer/ComputeCommandBuffer.h"
 
 namespace Engine {
     class ComputeContext : public ICommandContext {
         void UseImage(vk::Image img, ImageAccessType currentAccess, ImageAccessType previousAccess) noexcept override;
+        struct impl;
+        std::unique_ptr <impl> pimpl;
     public:
         using ImageComputeAccessType = AccessHelper::ImageComputeAccessType;
+
+        ComputeContext(ComputeCommandBuffer && cb);
+        virtual ~ComputeContext();
         
         /**
          * @brief Mark an image for use for the following compute context.
