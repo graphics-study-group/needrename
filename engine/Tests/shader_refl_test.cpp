469d6358b32bd123c2366fb4532050f6c8fbfab7
Lee <vincent_li_2024@126.com>
2025-09-12 19:21:41 +0800

    Fix SDL log not showing up

--- engine/Tests/shader_refl_test.cpp
+++ engine/Tests/shader_refl_test.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <filesystem>
 #include <cassert>
+#include <format>
+#include <SDL3/SDL.h>
 
 #include "Render/Memory/ShaderParameters/ShaderParameterLayout.h"
 #include "Render/Memory/ShaderParameters/ShaderParameterSimple.h"
 #include <cmake_config.h>
-#include <format>
 
 inline std::vector <uint32_t> ReadSpirvBinary(std::filesystem::path p) {
     std::vector <uint32_t> binary{};
@@ -62,8 +63,12 @@ int main(int argc, char *argv[]) {
     auto p = argc == 1 ? 
         std::filesystem::path(ENGINE_BUILTIN_ASSETS_DIR) / "shaders/lambertian_cook_torrance.frag.spv" 
         : std::filesystem::path(ENGINE_BUILTIN_ASSETS_DIR) / "shaders" / argv[1];
+
+    // Get SDL logging working.
+    SDL_SetLogPriorities(SDL_LOG_PRIORITY_VERBOSE);
+    
     auto binary = ReadSpirvBinary(p);
-    auto layout = Engine::ShdrRfl::SPLayout::Reflect(binary, true);
+    auto layout = Engine::ShdrRfl::SPLayout::Reflect(binary, false);
     PrintLayout(layout);
 
     binary = ReadSpirvBinary(std::filesystem::path(ENGINE_BUILTIN_ASSETS_DIR) / "shaders/pbr_base.vert.spv");
