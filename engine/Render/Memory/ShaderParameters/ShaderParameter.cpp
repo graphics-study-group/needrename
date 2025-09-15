b8253a63edf86c5319d7a81e62b98c84de04482a
Lee <vincent_li_2024@126.com>
2025-09-15 17:23:27 +0800

    Add ShaderParameter class

---
+++ engine/Render/Memory/ShaderParameters/ShaderParameter.cpp
@@ -0,0 +1,28 @@
+#include "ShaderParameter.h"
+
+namespace Engine::ShdrRfl {
+    void ShaderParameters::Assign(const std::string &name, std::variant<uint32_t, int32_t, float> value) noexcept {
+        if (std::holds_alternative<uint32_t>(value)) {
+            this->arguments[name] = std::get<uint32_t>(value);
+        } else if (std::holds_alternative<int32_t>(value)) {
+            this->arguments[name] = std::get<int32_t>(value);
+        } else if (std::holds_alternative<float>(value)) {
+            this->arguments[name] = std::get<float>(value);
+        } else {
+        }
+    }
+    void ShaderParameters::Assign(const std::string &name, std::variant<glm::vec4, glm::mat4> value) noexcept {
+        if (std::holds_alternative<glm::vec4>(value)) {
+            this->arguments[name] = std::get<glm::vec4>(value);
+        } else if (std::holds_alternative<glm::mat4>(value)) {
+            this->arguments[name] = std::get<glm::mat4>(value);
+        } else {
+        }
+    }
+    void ShaderParameters::Assign(const std::string &name, const Texture &value) noexcept {
+        this->interfaces[name] = std::cref(value);
+    }
+    void ShaderParameters::Assign(const std::string &name, const Buffer &buf, size_t offset, size_t size) noexcept {
+        this->interfaces[name] = std::make_tuple(std::cref(buf), offset, size);
+    }
+} // namespace Engine::ShdrRfl
--- engine/Render/Memory/ShaderParameters/ShaderParameter.h
+++ engine/Render/Memory/ShaderParameters/ShaderParameter.h
@@ -1,12 +1,49 @@
-#ifndef MEMORY_SHADERPARAMETERS_SHADERPARAMETERVALUE_INCLUDED
-#define MEMORY_SHADERPARAMETERS_SHADERPARAMETERVALUE_INCLUDED
+#ifndef MEMORY_SHADERPARAMETERS_SHADERPARAMETER_INCLUDED
+#define MEMORY_SHADERPARAMETERS_SHADERPARAMETER_INCLUDED
+
+#include <unordered_map>
+#include <string>
+#include <variant>
+
+#include <fwd.hpp>
 
 namespace Engine {
+    class Texture;
+    class Buffer;
     namespace ShdrRfl {
-        class ShaderParameters {
+        struct ShaderParameters {
+            using ParameterVariant = std::variant<
+                    std::monostate,
+                    uint32_t,
+                    int32_t,
+                    float,
+                    glm::vec4,
+                    glm::mat4
+                >;
+            using InterfaceVariant = std::variant<
+                    std::monostate,
+                    std::reference_wrapper<const Texture>,
+                    std::vector <std::reference_wrapper<const Texture>>,
+                    // Buffer, offset and size
+                    std::tuple<std::reference_wrapper<const Buffer>, size_t, size_t>
+                >;
+            std::unordered_map <std::string, ParameterVariant> arguments;
+            std::unordered_map <std::string, InterfaceVariant> interfaces;
 
+            void Assign(const std::string & name, std::variant<uint32_t, int32_t, float> value) noexcept;
+
+            void Assign(const std::string & name, std::variant<glm::vec4, glm::mat4> value) noexcept;
+
+            void Assign(const std::string & name, const Texture & value) noexcept;
+
+            void Assign(
+                const std::string & name, 
+                const Buffer & buf, 
+                size_t offset = 0ULL, 
+                size_t size = 0ULL
+            ) noexcept;
         };
     }
 }
 
-#endif // MEMORY_SHADERPARAMETERS_SHADERPARAMETERVALUE_INCLUDED
+#endif // MEMORY_SHADERPARAMETERS_SHADERPARAMETER_INCLUDED
