67b24bb38a647239b1b42cbe0df47574f2ef3135
Lee <vincent_li_2024@126.com>
2025-09-16 16:24:02 +0800

    Remove PipelineInfo interfaces

--- engine/Render/Pipeline/PipelineInfo.h
+++ engine/Render/Pipeline/PipelineInfo.h
@@ -36,53 +36,6 @@ namespace Engine {
                 vk::DescriptorPoolSize{vk::DescriptorType::eCombinedImageSampler, 64}
             };
         };
-
-        struct InstancedPassInfo {
-            // FIXME: We are only allocating one buffer for multiple frames-in-flight. This might lead to
-            // synchronization problems.
-            std::unique_ptr<Buffer> ubo{};
-            vk::DescriptorSet desc_set{};
-            bool is_ubo_dirty{false};
-            bool is_descriptor_set_dirty{false};
-        };
-
-        /**
-         * @brief Place UBO variables at given buffer, ready for directly writing into UBO.
-         * 
-
-         * * @remark For internal use only.
-         */
-        void PlaceUBOVariables(
-            const std::unordered_map<uint32_t, std::any> &variables,
-            const PassInfo &info,
-            std::vector<std::byte> &memory
-        ) noexcept;
-
-        /**
-         * @brief Get the descriptor image info for a specific variable set.
-         * 
-         * The
-         * layout for the image is selected based on its type.
-         * If the type is `Texture`, then the layout is
-         * `ReadOnlyOptimal`.
-         * If the type is `StorageImage`, then the layout is `General`.
-         * 
-
-         * * @remark For internal use only.
-         */
-        std::vector<std::pair<uint32_t, vk::DescriptorImageInfo>> GetDescriptorImageInfo(
-            const std::unordered_map<uint32_t, std::any> &variables, const PassInfo &info, vk::Sampler sampler
-        ) noexcept;
-
-        /**
-         * @brief Get the descriptor for storage buffers info for a specific variable set.
-         *
-         * @remark For internal use only.
-         * @warning unimplemented
-         */
-        std::vector<std::pair<uint32_t, vk::DescriptorBufferInfo>> GetDescriptorBufferInfo(
-            const std::unordered_map<uint32_t, std::any> &variables, const PassInfo &info
-        ) noexcept;
     } // namespace PipelineInfo
 } // namespace Engine
 
