dea7df8a2a8b354f8aa98c878c10996a48049580
CaptainHarryChen <captainharrychen@qq.com>
2026-01-10 22:39:54 +0800

    add shader includer

---
+++ engine/Asset/Shader/ShaderIncluder.h
@@ -0,0 +1,28 @@
+#ifndef ENGINE_ASSET_SHADER_SHADERINCLUDER_H
+#define ENGINE_ASSET_SHADER_SHADERINCLUDER_H
+
+#pragma once
+
+#include <glslang/Public/ShaderLang.h>
+#include <unordered_map>
+
+namespace Engine {
+    class ShaderIncluder : public glslang::TShader::Includer {
+        static const std::unordered_map<std::string, std::string> k_include_path_map;
+
+    public:
+        ShaderIncluder();
+        virtual ~ShaderIncluder();
+
+        virtual IncludeResult *includeLocal(
+            const char *headerName, const char *includerName, size_t inclusionDepth
+        ) override;
+        virtual IncludeResult *includeSystem(
+            const char *headerName, const char *includerName, size_t inclusionDepth
+        ) override;
+        virtual void releaseInclude(IncludeResult *result) override;
+    };
+
+} // namespace Engine
+
+#endif // ENGINE_ASSET_SHADER_SHADERINCLUDER_H
