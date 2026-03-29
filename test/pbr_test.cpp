3471d64aa745b988e3b30509e6e67c93b609a39b
Vincent Lee <vincent_li_2024@126.com>
2026-03-29 14:26:12 +0800

    Add resizable RTT test

--- test/pbr_test.cpp
+++ test/pbr_test.cpp
@@ -244,11 +244,11 @@ int main(int argc, char **argv) {
         .multisample = 1,
         .is_cube_map = false
     };
-    auto hc = rgb.RequestRenderTargetTexture(rtt_desc, Texture::SamplerDesc{});
+    auto hc = rgb.RequestResizableRenderTargetTexture(rtt_desc, Texture::SamplerDesc{});
     rtt_desc.format = RenderTargetTexture::RenderTargetTextureDesc::RTTFormat::D32SFLOAT;
-    auto d = rgb.RequestRenderTargetTexture(rtt_desc, Texture::SamplerDesc{});
+    auto d = rgb.RequestResizableRenderTargetTexture(rtt_desc, Texture::SamplerDesc{});
     rtt_desc.format = RenderTargetTexture::RenderTargetTextureDesc::RTTFormat::R8G8B8A8UNorm;
-    auto c = rgb.RequestRenderTargetTexture(rtt_desc, Texture::SamplerDesc{});
+    auto c = rgb.RequestResizableRenderTargetTexture(rtt_desc, Texture::SamplerDesc{});
     // Color pass
     using IAT = MemoryAccessTypeImageBits;
     rgb.AddPass(
