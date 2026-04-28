da4e19b278a7cb6a84971ff667282c83efa53b86
Lee <vincent_li_2024@126.com>
2026-04-28 13:34:08 +0800

    Rewrite allocator state

--- engine/Render/RenderSystem/AllocatorState.h
+++ engine/Render/RenderSystem/AllocatorState.h
@@ -5,20 +5,11 @@
 #include "Render/Memory/MemoryAllocation.h"
 #include "Render/Memory/MemoryTypes.h"
 #include <memory>
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan.hpp>
 
 class VkExtent3D;
 class VmaAllocator_T;
 typedef VmaAllocator_T *VmaAllocator;
-
-namespace vk {
-    enum class Format;
-    enum class ImageType;
-    enum class SampleCountFlagBits : uint32_t;
-    enum class FormatFeatureFlagBits : uint32_t;
-
-    class Extent3D;
-} // namespace vk
 
 namespace Engine {
     class RenderSystem;
@@ -39,9 +30,10 @@ namespace Engine {
             AllocatorState(RenderSystem &system);
 
             AllocatorState(const AllocatorState &) = delete;
-            AllocatorState(AllocatorState &&) = default;
             AllocatorState &operator=(const AllocatorState &) = delete;
-            AllocatorState &operator=(AllocatorState &) = default;
+
+            AllocatorState(AllocatorState &&) = default;
+            AllocatorState &operator=(AllocatorState &&) = default;
 
             ~AllocatorState();
             /// @brief Create the allocator state by initializing the VMA library.
@@ -55,39 +47,49 @@ namespace Engine {
             BufferAllocation AllocateBuffer(BufferType type, size_t size, const std::string &name = "") const;
 
             /**
-             * @overload BufferAllocation AllocatorState::AllocateBuffer()
+             * @overload BufferAllocation AllocatorState::AllocateBufferUnique()
+             * 
+             * This variant creates a unique pointer instead, and does not throw
+             * exceptions.
+             * 
+             * @see ImageAllocation AllocatorState::AllocateBuffer()
              */
             std::unique_ptr<BufferAllocation> AllocateBufferUnique(
                 BufferType type, size_t size, const std::string &name = ""
             ) const noexcept;
 
             /**
+             * @brief Description of image allocation request.
+             */
+            struct ImageAllocationDescription {
+                ImageMemoryType type;               ///< Supported memory access pattern of the image.
+                vk::ImageType dimension;            ///< Dimension of the image. 1D, 2D or 3D are supported.
+                vk::Extent3D extent;                ///< Extent of the image. Must be non-zero integers.
+                vk::Format format;                  ///< Format of the image.
+                uint32_t miplevel;                  ///< Mipmap levels of the image.
+                uint32_t array_layers;              ///< Array layers of the image. 3D arrays are not supported.
+                bool is_cube_map;                   ///< Whether the array allocation should be compatible to cubemaps.
+                vk::SampleCountFlagBits samples;    ///< Multisample counts of the image.
+            };
+
+            /**
              * @brief Allocate the memory for a given image.
              */
             ImageAllocation AllocateImage(
-                ImageMemoryType type,
-                vk::ImageType dimension,
-                vk::Extent3D extent,
-                vk::Format format,
-                uint32_t miplevel,
-                uint32_t array_layers,
-                bool is_cube_map,
-                vk::SampleCountFlagBits samples,
+                const ImageAllocationDescription & desc,
                 const std::string &name = ""
             ) const;
 
             /**
-             * @overload ImageAllocation AllocatorState::AllocateImage()
+             * @overload ImageAllocation AllocatorState::AllocateImageUnique()
+             * 
+             * This variant creates a unique pointer instead, and does not throw
+             * exceptions.
+             * 
+             * @see ImageAllocation AllocatorState::AllocateImage()
              */
             std::unique_ptr<ImageAllocation> AllocateImageUnique(
-                ImageMemoryType type,
-                vk::ImageType dimension,
-                vk::Extent3D extent,
-                vk::Format format,
-                uint32_t miplevel,
-                uint32_t array_layers,
-                bool is_cube_map,
-                vk::SampleCountFlagBits samples,
+                const ImageAllocationDescription & desc,
                 const std::string &name = ""
             ) const noexcept;
 
