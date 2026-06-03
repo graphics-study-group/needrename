a4a1b1031b95eb2acfdbdff7014abc4f2136fc67
Lee <vincent_li_2024@126.com>
2026-06-03 17:21:46 +0800

    Update test of renderer thread

--- engine/Tests/renderer_thread_test.cpp
+++ engine/Tests/renderer_thread_test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Core/Functional/SDLWindow.h"
 #include "Render/FullRenderSystem.h"
+#include "Render/RenderThread/RenderServiceQueue.h"
 #include "Render/RenderThread/RenderThread.h"
 #include "Render/RenderThread/RenderThreadState.h"
 #include "Render/RenderThread/Tasks/RenderTasks.h"
@@ -15,11 +16,53 @@ int main() {
     constexpr auto sdl_window_flags = SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
     auto window = std::make_shared<SDLWindow>("TEST", 400, 300, sdl_window_flags);
 
+    auto rgb = std::make_unique<RenderGraphBuilder2>();
+
+    auto rttd = RenderTargetTexture::RenderTargetTextureDesc{
+        .dimensions = 2,
+        .width = 400,
+        .height = 300,
+        .depth = 1,
+        .mipmap_levels = 1,
+        .array_layers = 1,
+        .format = RenderTargetTexture::RTTFormat::R8G8B8A8UNorm,
+        .multisample = 1,
+    };
+    auto mbuffer = rgb->RequestRenderTargetTexture(rttd, {}, "Main buffer");
+    rgb->AddPass(
+        RenderGraphPassBuilder{}
+            .SetName("Main pass")
+            .AppendColorAttachment(
+                {
+                    mbuffer,
+                    {},
+                    AttachmentUtils::LoadOperation::Clear,
+                    AttachmentUtils::StoreOperation::Store,
+                    {
+                        AttachmentUtils::ColorClearValue{1.0f, 0.0f, 0.0f, 1.0f}
+                    }
+                }
+            )
+            .SetRasterizerPassFunction([](
+                GraphicsCommandBuffer &,
+                const RenderGraph2 &
+            ){})
+            .WrapRenderPass()
+            .Get()
+    );
+
     auto thd = RenderThread{window};
     auto & s = *thd.BlockAndAcquireThreadState().render_system;
     auto p = s.GetDeviceInterface().GetPhysicalDevice().getProperties();
-    
     std::cout << p.deviceName << "\n";
+
+    thd.Resume();
+    thd.GetServiceQueue().push(
+        std::make_unique<RenderTasks::RenderTaskBuildActiveRenderGraph>(std::move(rgb))
+    );
+    thd.GetServiceQueue().push(
+        std::make_unique<RenderTasks::RenderTaskRenderOneFrameWithActiveGraph>(mbuffer)
+    );
 
     return 0;
 }
